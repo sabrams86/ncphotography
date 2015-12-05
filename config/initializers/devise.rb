2152eae8836e4e3d285c1a206432e10b9022b0bb
Steven Abrams <steven.abrams86@gmail.com>
2015-12-05 09:41:49 -0700

    fixed precompile issue

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  config.secret_key = '9111affefd7e28b5b11e0f7bbe1a677ee98784c89e1b0a81595b52536eadb49934014eae7f7f096a28c972c659b13c49940aa189e578ee00bd7e4260f2da417b'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
